0a3d3eb6c0a582243f18c783b659ab04d9117d85
freeridersochi <demitryvoit@gmail.com>
2020-11-16 00:35:37 +0300

    create  v2_brunch

---
+++ jumbotron_btn.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const e = React.createElement;
+
+class JumbotronButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('Junbotron_btn');
+ReactDOM.render(e(jumbotronButton), domContainer);
+
